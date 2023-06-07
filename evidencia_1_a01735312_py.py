82ef27f5165be9cc973a05c568b1fcaae1accd5d
ABNERP12 <131408424+ABNERP12@users.noreply.github.com>
2023-06-06 20:07:47 -0600

    Update evidencia_1_a01735312_py.py

--- evidencia_1_a01735312_py.py
+++ evidencia_1_a01735312_py.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH0e9/view?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv")
+df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
@@ -31,11 +31,18 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view?usp=sharing")
+
+df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
+
+st.dataframe(df)
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa = pd.DataFrame()
+mapa=pd.DataFrame(df['Latitude'], df['Longitude'])
+mapa=mapa.dropna()
+st.map(mapa.astype(int))
+
+"""mapa = pd.DataFrame()
 mapa['Date'] = df['Incident Date']
 mapa['Day'] = df['Incident Day of Week']
 mapa['Police District'] = df['Police District']
